7f063db32f1b8ee8f5fa8ac69ce2221fd2219808
martinbaud <martin.baud@epitech.eu>
2017-11-09 17:13:19 +0100

    Command PLAY
    Add of the command PLAY

--- Jcoinche/ClientApplication/Program.cs
+++ Jcoinche/ClientApplication/Program.cs
@@ -13,6 +13,7 @@ namespace ClientApplication
         static ServerInfo serv = new ServerInfo();
         static private bool end = false;
         static private bool once = false;
+        static int PlayerId;
         static void Main(string[] args)
         {
             NetworkComms.AppendGlobalIncomingPacketHandler<PlayerObject>("Message", PrintIncomingMessage);
@@ -32,7 +33,13 @@ namespace ClientApplication
                     NetworkComms.SendObject("Message", serv.serverIp, serv.serverPort, message);
                     once = true;
                 }
-               
+                if (Equals(Console.ReadLine(), "PLAY") == true)
+                {
+                    PlayerObject message = new PlayerObject(20, tmp, "Play from player "+PlayerId);
+                    Console.WriteLine(PlayerId);
+                    NetworkComms.SendObject("Message", serv.serverIp, serv.serverPort, message);
+                    Console.WriteLine("ok");
+                }
 
                 Console.WriteLine("\nPress q to quit or any other key to send another message.");
                 if (Console.ReadKey(true).Key == ConsoleKey.Q) break;
@@ -42,12 +49,12 @@ namespace ClientApplication
         }
         private static void PrintIncomingMessage(PacketHeader header, Connection connection, PlayerObject player)
         {
-            Console.WriteLine(player.msg);
             Hand _hand = new Hand();
             if (Equals(player.Id, 1) == true)
             {
                 Console.WriteLine(player.msg);
                 _hand.HandPlayer1 = player.Hand;
+                tmp = player.Hand;
                 PrintCollection(_hand.HandPlayer1);
                 PlayerObject message = new PlayerObject(10, tmp, "Hand received from player1");
                 NetworkComms.SendObject("Message", serv.serverIp, serv.serverPort, message);
@@ -56,9 +63,22 @@ namespace ClientApplication
             {
                 Console.WriteLine(player.msg);
                 _hand.HandPlayer2 = player.Hand;
+                tmp = player.Hand;
                 PrintCollection(_hand.HandPlayer2);
                 PlayerObject message = new PlayerObject(10, tmp, "Hand received from player2");
                 NetworkComms.SendObject("Message", serv.serverIp, serv.serverPort, message);
+            }
+            if (Equals(player.Id, 10) == true)
+            {
+                Console.WriteLine(player.msg);
+            }
+            if (Equals(player.msg, "The game can begin, anytime it's your turn to play just write PLAY p1") == true)
+            {
+                PlayerId = 1;
+            }
+            if (Equals(player.msg, "The game can begin, anytime it's your turn to play just write PLAY p2") == true)
+            {
+                PlayerId = 2;
             }
         }
         public static void PrintCollection<T>(IEnumerable<T> col)
